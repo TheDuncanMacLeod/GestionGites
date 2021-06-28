5394843242a85c94f2be6533aafdfbf75f6e20a0
R Gael <r.gael01@gmail.com>
2021-06-28 12:54:49 +0200

    Validation des donnees sur GiteType

--- src/Form/GiteType.php
+++ src/Form/GiteType.php
@@ -16,23 +16,91 @@ class GiteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Name', TextType::class, 
-            ['required' => false ])
-            ->add('Description', TextareaType::class, 
-            ['required' => false ])
-            ->add('Surface', NumberType::class, 
-            ['required' => false ])
-            ->add('Bedrooms', NumberType::class, 
-            ['required' => false ])
-            ->add('Adress', TextType::class, 
-            ['required' => false ])
-            ->add('City', TextType::class, 
-            ['required' => false ])
-            ->add('Postal_code', TextType::class, 
-            ['required' => false ])
-            ->add('Animals', CheckboxType::class, 
-            ['required' => false ])
-        ;
+            ->add(
+                'Name',
+                TextType::class,
+                [
+                    'required' => false,
+                    'label' => 'Nom du gite',
+                    'attr' => [
+                        'placeholder' => 'Entrer le nom du gite',
+                    ]
+                ]
+            )
+            ->add(
+                'Description',
+                TextareaType::class,
+                [
+                    'required' => false,
+                    'label' => 'Description du gite',
+                    'attr' => [
+                        'placeholder' => 'Entrer une description du gite',
+                    ]
+                ]
+            )
+            ->add(
+                'Surface',
+                NumberType::class,
+                [
+                    'required' => false,
+                    'label' => 'Surface',
+                    'attr' => [
+                        'placeholder' => 'Entrer la surface du gite',
+                    ]
+                ]
+            )
+            ->add(
+                'Bedrooms',
+                NumberType::class,
+                [
+                    'required' => false,
+                    'label' => 'Nombre de chambres',
+                    'attr' => [
+                        'placeholder' => 'Entrer le nombre de chambres',
+                    ]
+                ]
+            )
+            ->add(
+                'Adress',
+                TextType::class,
+                [
+                    'required' => false,
+                    'label' => 'Adresse du gite',
+                    'attr' => [
+                        'placeholder' => 'Entrer l\'adresse du gite',
+                    ]
+                ]
+            )
+            ->add(
+                'City',
+                TextType::class,
+                [
+                    'required' => false,
+                    'label' => 'Ville',
+                    'attr' => [
+                        'placeholder' => 'Entrer le nom de la ville',
+                    ]
+                ]
+            )
+            ->add(
+                'Postal_code',
+                TextType::class,
+                [
+                    'required' => false,
+                    'label' => 'Code postal',
+                    'attr' => [
+                        'placeholder' => 'Entrer le code postal',
+                    ]
+                ]
+            )
+            ->add(
+                'Animals',
+                CheckboxType::class,
+                [
+                    'required' => false,
+                    'label' => 'Animaux accepté',
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
